3d30ab0a5a854c21055fa8538ce8ce21d039cf1e
dopereff <92185890+dopereff@users.noreply.github.com>
2021-11-17 20:18:40 +0200

    Add sleepDept.js
    A program calculating if you are gettin enough sleep.

---
+++ sleepDept.js
@@ -0,0 +1,55 @@
+const getSleepHours = (day) => {
+  switch (day) {
+    case "monday":
+      return 8;
+    case "tuesday":
+      return 5;
+    case "wednesday":
+      return 6;
+    case "thursday":
+      return 9;
+    case "friday":
+      return 10;
+    case "saturday":
+      return 5;
+    case "sunday":
+      return 12;
+  }
+};
+
+let getActualSleepHours = () => {
+  return (
+    getSleepHours("monday") +
+    getSleepHours("tuesday") +
+    getSleepHours("wednesday") +
+    getSleepHours("thursday") +
+    getSleepHours("friday") +
+    getSleepHours("saturday") +
+    getSleepHours("sunday")
+  );
+};
+
+let getIdealSleepHours = () => {
+  const idealHours = 8;
+  return idealHours * 7;
+};
+
+function calculateSleepDebt() {
+  actualSleepHours = getActualSleepHours();
+  idealSleepHours = getIdealSleepHours();
+  let sleepDept = Math.abs(idealSleepHours - actualSleepHours);
+
+  if (actualSleepHours === idealSleepHours) {
+    console.log("Bravo mashina, spal si dostutchno!");
+  } else if (actualSleepHours < idealSleepHours) {
+    console.log(
+      `Mashinka, trqbva da spish ${sleepDept} chasa poveche na sedmica!`
+    );
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log(
+      `Mnogo spish, ve mashina! Taq sedmica si spal s ${sleepDept} chasa poveche ot kolkot trqbva!`
+    );
+  }
+}
+
+calculateSleepDebt();

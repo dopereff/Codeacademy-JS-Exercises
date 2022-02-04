0062c91c9e0b1dd156352ddc727bb35f44168b03
dopereff <92185890+dopereff@users.noreply.github.com>
2022-02-04 17:40:31 +0200

    refactoring

--- ShiftCipher.js
+++ ShiftCipher.js
@@ -2,66 +2,84 @@
 class ShiftCipher {
   constructor(shiftValue) {
     this.shiftValue = shiftValue;
+
+    this.charCodeFirstCapLetter = 65;
+    this.charCodeLastCapLetter = 90;
+
+    this.isCapLetter = (charCode) => {
+      if (charCode >= this.charCodeFirstCapLetter && charCode <= this.charCodeLastCapLetter) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    // this.addToString = (str, charCode) => {
+    //   str += String.fromCharCode(charCode);
+    //   return str;
+    // };
+
+    // this.wrapAroundAlphabet = (charCode) => {
+    //   if (charCode > this.charCodeLastCapLetter) {
+    //     const difference = charCode - this.charCodeLastCapLetter;
+    //     charCode = this.charCodeFirstCapLetter + difference - 1;
+    //   } else {
+    //     const difference = this.charCodeFirstCapLetter - charCode;
+    //     charCode = this.charCodeLastCapLetter - difference + 1;
+    //   }
+    // };
   }
+
   encrypt(str) {
     str = str.toUpperCase();
     let encryptedStr = "";
-    let charCodeOfFirstCapLetter = 65;
-    let charCodeOfLastCapLetter = 90;
 
     for (let i = 0; i < str.length; i++) {
       let currentLetterCharCode = str.charCodeAt(i);
 
-      if (currentLetterCharCode >= charCodeOfFirstCapLetter && currentLetterCharCode <= charCodeOfLastCapLetter) {
+      if (this.isCapLetter(currentLetterCharCode)) {
         currentLetterCharCode += this.shiftValue;
-        let isCapLetter = currentLetterCharCode - charCodeOfLastCapLetter <= 0;
 
-        if (!isCapLetter) {
-          currentLetterCharCode = charCodeOfFirstCapLetter + (currentLetterCharCode - charCodeOfLastCapLetter + 1);
+        if (!this.isCapLetter(currentLetterCharCode)) {
+          // this.wrapAroundAlphabet(currentLetterCharCode);
+          const difference = currentLetterCharCode - this.charCodeLastCapLetter;
+          currentLetterCharCode = this.charCodeFirstCapLetter + difference - 1;
         }
-
-        // fromCharToLetter(currentLetterCharCode, encryptedStr);
+        // this.addToString(encryptedStr, currentLetterCharCode);
         encryptedStr += String.fromCharCode(currentLetterCharCode);
       } else {
-        // fromCharToLetter(currentLetterCharCode, encryptedStr);
+        // this.addToString(encryptedStr, currentLetterCharCode);
         encryptedStr += String.fromCharCode(currentLetterCharCode);
       }
     }
-
     return encryptedStr;
   }
 
   decrypt(str) {
-    let charCodeOfFirstCapLetter = 65;
-    let charCodeOfLastCapLetter = 90;
     let decryptedString = "";
 
     for (let i = 0; i < str.length; i++) {
       let currentLetterCharCode = str.charCodeAt(i);
 
-      if (currentLetterCharCode >= charCodeOfFirstCapLetter && currentLetterCharCode <= charCodeOfLastCapLetter) {
+      if (this.isCapLetter(currentLetterCharCode)) {
         currentLetterCharCode -= this.shiftValue;
-        let isCapLetter = currentLetterCharCode - charCodeOfFirstCapLetter >= 0;
 
-        if (!isCapLetter) {
-          currentLetterCharCode = charCodeOfLastCapLetter + (currentLetterCharCode - charCodeOfFirstCapLetter + 1);
+        if (!this.isCapLetter(currentLetterCharCode)) {
+          // this.wrapAroundAlphabet(currentLetterCharCode);
+          const difference = this.charCodeFirstCapLetter - currentLetterCharCode;
+          currentLetterCharCode = this.charCodeLastCapLetter - difference + 1;
         }
-
-        // fromCharToLetter(currentLetterCharCode, encryptedStr);
+        // this.addToString(decryptedString, currentLetterCharCode);
         decryptedString += String.fromCharCode(currentLetterCharCode);
       } else {
-        // fromCharToLetter(currentLetterCharCode, encryptedStr);
+        // this.addToString(decryptedString, currentLetterCharCode);
         decryptedString += String.fromCharCode(currentLetterCharCode);
       }
     }
     return decryptedString.toLowerCase();
   }
 }
-// const fromCharToLetter = (charCode, str) => {
-//   let letter = String.fromCharCode(charCode);
-//   str += letter;
-// };
 
 const cipher = new ShiftCipher(2);
-console.log(cipher.encrypt("I love to code!")); // returns 'K NQXG VQ EQFG!'
-console.log(cipher.decrypt('K <3 OA RWRRA')); // returns 'i <3 my puppy'
+console.log(cipher.encrypt("I love to codez!")); // returns 'K NQXG VQ EQFG!'
+console.log(cipher.decrypt("K <3 OA RWRRA")); // returns 'i <3 my puppy'

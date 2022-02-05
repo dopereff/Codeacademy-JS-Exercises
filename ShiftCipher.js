b72301bc3576777ef9c947999f8e6572d0eba74a
dopereff <92185890+dopereff@users.noreply.github.com>
2022-02-05 10:49:15 +0200

    refactoring

--- ShiftCipher.js
+++ ShiftCipher.js
@@ -2,33 +2,25 @@
 class ShiftCipher {
   constructor(shiftValue) {
     this.shiftValue = shiftValue;
-
-    this.charCodeFirstCapLetter = 65;
-    this.charCodeLastCapLetter = 90;
-
-    this.isCapLetter = (charCode) => {
-      if (charCode >= this.charCodeFirstCapLetter && charCode <= this.charCodeLastCapLetter) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    // this.addToString = (str, charCode) => {
-    //   str += String.fromCharCode(charCode);
-    //   return str;
-    // };
-
-    // this.wrapAroundAlphabet = (charCode) => {
-    //   if (charCode > this.charCodeLastCapLetter) {
-    //     const difference = charCode - this.charCodeLastCapLetter;
-    //     charCode = this.charCodeFirstCapLetter + difference - 1;
-    //   } else {
-    //     const difference = this.charCodeFirstCapLetter - charCode;
-    //     charCode = this.charCodeLastCapLetter - difference + 1;
-    //   }
-    // };
   }
+
+  charCodeFirstCapLetter = 65;
+  charCodeLastCapLetter = 90;
+
+  isCapLetter = (charCode) => charCode >= this.charCodeFirstCapLetter && charCode <= this.charCodeLastCapLetter;
+
+  wrapAroundAlphabet = (charCode) => {
+    if (charCode > this.charCodeLastCapLetter) {
+      const difference = charCode - this.charCodeLastCapLetter;
+      charCode = this.charCodeFirstCapLetter + difference - 1;
+    } else {
+      const difference = this.charCodeFirstCapLetter - charCode;
+      charCode = this.charCodeLastCapLetter - difference + 1;
+    }
+    return charCode;
+  };
+
+  toLetterFromCharCode = (charCode) => String.fromCharCode(charCode);
 
   encrypt(str) {
     str = str.toUpperCase();
@@ -41,22 +33,15 @@ class ShiftCipher {
         currentLetterCharCode += this.shiftValue;
 
         if (!this.isCapLetter(currentLetterCharCode)) {
-          // this.wrapAroundAlphabet(currentLetterCharCode);
-          const difference = currentLetterCharCode - this.charCodeLastCapLetter;
-          currentLetterCharCode = this.charCodeFirstCapLetter + difference - 1;
+          currentLetterCharCode = this.wrapAroundAlphabet(currentLetterCharCode);
         }
-        // this.addToString(encryptedStr, currentLetterCharCode);
-        encryptedStr += String.fromCharCode(currentLetterCharCode);
-      } else {
-        // this.addToString(encryptedStr, currentLetterCharCode);
-        encryptedStr += String.fromCharCode(currentLetterCharCode);
       }
+      encryptedStr += this.toLetterFromCharCode(currentLetterCharCode);
     }
     return encryptedStr;
   }
 
   decrypt(str) {
-    str = str.toUpperCase();
     let decryptedString = "";
 
     for (let i = 0; i < str.length; i++) {
@@ -66,18 +51,11 @@ class ShiftCipher {
         currentLetterCharCode -= this.shiftValue;
 
         if (!this.isCapLetter(currentLetterCharCode)) {
-          // this.wrapAroundAlphabet(currentLetterCharCode);
-          const difference = this.charCodeFirstCapLetter - currentLetterCharCode;
-          currentLetterCharCode = this.charCodeLastCapLetter - difference + 1;
+          currentLetterCharCode = this.wrapAroundAlphabet(currentLetterCharCode);
         }
-        // this.addToString(decryptedString, currentLetterCharCode);
-        decryptedString += String.fromCharCode(currentLetterCharCode);
-      } else {
-        // this.addToString(decryptedString, currentLetterCharCode);
-        decryptedString += String.fromCharCode(currentLetterCharCode);
       }
+      decryptedString += this.toLetterFromCharCode(currentLetterCharCode);
     }
     return decryptedString.toLowerCase();
   }
 }
-
